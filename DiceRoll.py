f290a97f5fb5005adcb5613ec87b394b55c142ce
Jesper <ajesper00@gmail.com>
2017-06-17 16:58:25 +0200

    Stuff

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,3 +1,10 @@
 # Random number generation between 1-6
 from random import randint
-print(randint(1, 6), randint(1, 6))
+
+print("Type in 'roll' to roll the dice...")
+rollprompt = raw_input()
+
+if rollprompt == "roll":
+    print(randint(1, 6), randint(1, 6))
+else:
+    raise SystemExit
