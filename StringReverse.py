765de7f4ece2191faeebd53576248fbaecf92f47
Jesper <ajesper00@gmail.com>
2017-07-14 07:42:48 +0200

    Comments.

--- StringReverse.py
+++ StringReverse.py
@@ -1,6 +1,7 @@
-# Grabs string input and reverses it and outputs it.
+# By: Jesper-Andersson (2017/?/?)
+# "Grabs string input and reverses it and outputs it."
 
 print("Type in your word/sentance:")
 word = raw_input()
 
-print(word[::-1])
+print(word[::-1]) # Magic
