98fd87ae94ed4edf456a7bf8d5d56857f834481e
Jesper-Andersson <ajesper00@gmail.com>
2017-07-14 07:33:47 +0200

    Did some stuff

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,10 +1,21 @@
 # Random number generation between 1-6
 from random import randint
+diceRoll = randint(1, 6)
 
-print("Type in 'roll' to roll the dice...")
-rollprompt = raw_input()
+print("DiceRoll - Jesper Andersson")
+print("")
+print("Enter the amount of dice to be rolled:")
+print("1 - 2 - 3")
+# Saves amount of dice to be rolled.
+diceAmount = raw_input()
 
-if rollprompt == "roll":
-    print(randint(1, 6), randint(1, 6))
+
+if diceAmount == 1
+    print(diceRoll)
 else:
-    raise SystemExit
+    if diceAmount == 2
+        print(diceRoll, diceRoll)
+    else:
+        if diceAmount == 3
+            print(diceRoll, diceRoll, diceRoll)
+        else: SystemExit
