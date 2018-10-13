d30fa27767d49304afc1185b5af71540f152caf6
Jesper <0008anje@edu.jonkoping.se>
2018-10-13 18:57:04 +0200

    Fix functions (First "working" version)

--- snakegame/snake.py
+++ snakegame/snake.py
@@ -2,62 +2,93 @@
 
 # TODO:
 # Background keyboard input
+# Unpassable borders
+# Game over
 # Apples & point display
 # "Snake" part
+# Remove empty line between grid rows
 
 import time
 import math
 from os import system, name
+import pyHook
 
 snake = "+"
 background = "-"
 apple = "O"
 
-grid = [background, background, background,
-		background, snake, background,
-		background, background, background]
+grid = [background, background, background, background,
+		background, snake, 		background, background,
+		background, background, background, background,
+		background, background, background, background
+		]
 
-old_pos = 0
 snake_position = 4
-grid_width = math.sqrt(len(grid))
+total_score = 0
+score_worth = 1
+
+grid_width = int(math.sqrt(len(grid)))
+print(str(grid_width))
+
+def clear(): 
+    _ = system('cls')
 
 def draw_grid():
+	clear()
+	scoreboard()
 	i = 0
-	while i < 9:
+	while i < len(grid):
 		print(grid[i], end='')
 		i += 1
 		if i % grid_width == 0:
 			print('\n')
 
-def move_up(snake_position, grid_width, old_pos):
-	old_pos = snake_position
-	snake_position = snake_position - grid_width
-def move_down(snake_position, grid_width, old_pos):
-	old_pos = snake_position
-	snake_position = snake_position + grid_width
-def move_left(snake_position, old_pos):
-	old_pos = snake_position
-	snake_position = snake_position - 1
-def move_right(snake_position, old_pos):
-	old_pos = snake_position
-	snake_position = snake_position + 1
+def update_movement(old_pos):
+	global grid
+	global snake_position
 
-def scoreboard():
-	pass
-
-def update_movement(snake_position, old_pos, grid, snake, background):
 	grid[snake_position] = snake
 	grid[old_pos] = background
-	clear()
 	draw_grid()
 
-def clear(): 
-    _ = system('cls')
+def move_up():
+	global snake_position
+
+	old_pos = snake_position
+	snake_position = snake_position - grid_width
+	update_movement(old_pos)
+
+def move_down():
+	global snake_position
+
+	old_pos = snake_position
+	snake_position = snake_position + grid_width
+	update_movement(old_pos)
+
+def move_left():
+	global snake_position
+
+	old_pos = snake_position
+	snake_position = snake_position - 1
+	update_movement(old_pos)
+
+def move_right():
+	global snake_position
+
+	old_pos = snake_position
+	snake_position = snake_position + 1
+	update_movement(old_pos)
+
+def scoreboard():
+	global total_score
+	print('Your score is: ' + str(total_score))
+
+def addscore():
+	global score_worth
+	global total_score
+	total_score += score_worth
 
 #Frontend stuff
-
-clear()
 draw_grid()
-time.sleep(5)
-clear()
-time.sleep(5)
+move_right()
+move_right()
