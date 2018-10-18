2671a31eaa77cfdc46f53a444408587d95b294cf
Jesper <0008anje@edu.jonkoping.se>
2018-10-18 17:48:46 +0200

    test

--- python/stringreverse/StringReverse.py
+++
@@ -1,9 +0,0 @@
-# By: Jesper-Andersson (2017/?/?)
-# "Grabs string input and reverses it and outputs it."
-
-print("Type in your word/sentence:")
-
-word = input() 
-reversedString = word[::-1]
-
-print(reversedString)
